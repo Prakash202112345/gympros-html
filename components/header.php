1a32047ed15f0d8ace470fd25fe119588759cb16
Prakash Sharma <prakashsharma@Prakashs-MacBook-Air.local>
2024-07-27 10:11:04 +0530

    setup

---
+++ components/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GymPros</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./font/style.css">
+</head>
+<body>
+    <header>
+        <div class="header-highlight">
+<div class="row">
+    <div class="col-md-2 mx-auto text-center">
+    Call 888-502-2348 – Shop Our 2024 New Year, New You Sale! – Save Up To 70%!
+    </div>
+</div>
+        </div>
+    </header>
--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Welcome to world
-    jfjf
-</body>
-</html>
+<?php
+include('./components/header.php')
+?>
+
+
+
+
+
+
+
+<?php
+include('./components/footer.php')
+?>
